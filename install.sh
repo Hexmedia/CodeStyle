75ecce3151ccdf10ac5b6aca35a326b9de27ef88
Krystian Kuczek <krystian@hexmedia.pl>
2016-02-13 18:14:02 +0100

    Installation and spaces.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d "./vendor" ]; then
+    vendor="vendor/"
+fi
+
+cp -r "$vendor/escapestudios/symfony2-coding-standard/Symfony2" "$vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/"
