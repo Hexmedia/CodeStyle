a8641797d1a13017f85d98815b659ca736d73318
Krystian Kuczek <krystian@hexmedia.pl>
2016-02-13 18:28:24 +0100

    Fixed install.

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+vendor=""
+
 if [ -d "./vendor" ]; then
     vendor="vendor/"
 fi
 
-cp -r "$vendor/escapestudios/symfony2-coding-standard/Symfony2" "$vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/"
+echo "`pwd`"
+
+if [ "$vendor" != "" ]; then
+    echo "Installing Symfony2."
+    cp -r "$vendor/escapestudios/symfony2-coding-standard/Symfony2" "$vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/"
+fi
